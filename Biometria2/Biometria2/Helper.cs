e4c6938179beafc3c6fe649ac416e89dea88be3d
sidleniwiec17 <piotrmichalik17@wp.pl>
2018-03-15 21:25:35 +0100

    initial commit 2 with basic functionality

---
+++ Biometria2/Biometria2/Helper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Biometria2
+{
+    public class Helper
+    {
+        public static System.Windows.Media.ImageSource ToBitmapSource(Bitmap p_bitmap)
+        {
+            IntPtr hBitmap = p_bitmap.GetHbitmap();
+            System.Windows.Media.ImageSource wpfBitmap;
+            try
+            {
+                wpfBitmap = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                //p_bitmap.Dispose();
+            }
+            return wpfBitmap;
+        }        
+
+        public static void GrayScale(BitmapTable btm)
+        {
+            for (int x = 0; x < btm.Width; x++)
+            {
+                for (int y = 0; y < btm.Height; y++)
+                {
+                    System.Drawing.Color oldColour;
+                    oldColour = btm.getPixel(x, y);
+                    var value = (0.2 * (double)oldColour.R) + (0.7 * (double)oldColour.G) + (0.1 * (double)oldColour.B);
+                    btm.setPixel(x, y, System.Drawing.Color.FromArgb((int)value, (int)value, (int)value));
+                }
+            }
+        }
+
+        public static void Gauss(BitmapTable btm)
+        {
+            double a = 2;
+            double[,] wages = new double[3, 3] { { 1, a, 1 }, { a, a * a, a }, { 1, a, 1 } };
+            GaussFilter(btm, a, wages);
+        }
+
+        public static void GaussFilter(BitmapTable btm, double a, double[,] wages)
+        {
+            BitmapTable tempPict = new BitmapTable(btm);
+
+            for (int x = 0; x < tempPict.Width; x++)
+            {
+                for (int y = 0; y < tempPict.Height; y++)
+                {
+                    double sumR = 0.0;
+                    double sumG = 0.0;
+                    double sumB = 0.0;
+                    double dividor = 0.0;
+                    for (int x2 = -1; x2 < 2; x2++)
+                    {
+                        for (int y2 = -1; y2 < 2; y2++)
+                        {
+                            if (x + x2 >= 0 && y + y2 >= 0 && x + x2 < tempPict.Width && y + y2 < tempPict.Height)
+                            {
+                                double currWage = wages[x2 + 1, y2 + 1];
+                                sumR += tempPict.getPixel(x + x2, y + y2).R * currWage;
+                                sumG += tempPict.getPixel(x + x2, y + y2).G * currWage;
+                                sumB += tempPict.getPixel(x + x2, y + y2).B * currWage;
+                                dividor += currWage;
+                            }
+                        }
+                    }
+                    btm.setPixel(x, y, System.Drawing.Color.FromArgb(FromInterval((int)(sumR / dividor)), FromInterval((int)(sumG / dividor)), FromInterval((int)(sumB / dividor))));
+                }
+            }
+        }
+
+        private static int FromInterval(int col)
+        {
+            if (col <= 255 && col >= 0)
+                return col;
+            else if (col > 255)
+                return 255;
+            else
+                return 0;
+        }
+    }
+}
--- Biometria2/Biometria2/MainWindow.xaml.cs
+++ Biometria2/Biometria2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,107 @@ namespace Biometria2
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Bitmap newBmp;
+        public BitmapTable newBmpTbl;
+        public Bitmap originalBitmap;
+        public BitmapTable originalBitmapTbl;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Load_Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "pic";
+            dialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png;*.bmp|" +
+                "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                "Portable Network Graphic (*.png)|*.png";
+
+            if (dialog.ShowDialog() == true)
+            {
+                BlakWait.Visibility = Visibility.Visible;
+                await LoadImages(new BitmapImage(new Uri(dialog.FileName)), dialog.FileName);
+                BlakWait.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async Task LoadImages( BitmapImage btmi, string FileName)
+        {
+            img.Source = btmi;
+            ori.Source = btmi;
+
+            await Task.Run(() => performLoadingPictures(btmi, FileName));
+        }
+
+        private void performLoadingPictures(BitmapImage btmi, string FileName)
+        {
+            newBmp = (Bitmap)Bitmap.FromFile(FileName);
+            originalBitmap = (Bitmap)Bitmap.FromFile(FileName);
+            newBmpTbl = new BitmapTable(newBmp);
+            originalBitmapTbl = new BitmapTable(newBmpTbl);
+        }
+
+        private async void GrayScale_Button(object sender, RoutedEventArgs e)
+        {
+            if (!(newBmp != null && newBmpTbl != null))
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            await RunGreyScale();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = newBmpTbl.ToBitmapSource();
+            Console.WriteLine("Gray Scale.");
+        }
+
+        private async void Gauss_Button(object sender, RoutedEventArgs e)
+        {
+            if (!(newBmp != null && newBmpTbl != null))
+            {
+                MessageBox.Show("Load image!");
+                return;
+            }
+            BlakWait.Visibility = Visibility.Visible;
+            await RunGaussFilter();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = newBmpTbl.ToBitmapSource();
+            Console.WriteLine("Gauss Filter.");
+        }
+
+        private async void Clear_Button(object sender, RoutedEventArgs e)
+        {
+            BlakWait.Visibility = Visibility.Visible;
+            await ClearPic();
+            BlakWait.Visibility = Visibility.Collapsed;
+            img.Source = newBmpTbl.ToBitmapSource();
+        }
+
+        public async Task ClearPic()
+        {
+            await Task.Run(() =>
+            {
+                newBmpTbl = new BitmapTable(originalBitmapTbl);
+            });
+        }
+
+        public async Task RunGreyScale()
+        {
+            await Task.Run(() =>
+            {
+                Helper.GrayScale(newBmpTbl);
+            });
+        }
+
+        public async Task RunGaussFilter()
+        {
+            await Task.Run(() =>
+            {
+                Helper.Gauss(newBmpTbl);
+            });
         }
     }
 }
