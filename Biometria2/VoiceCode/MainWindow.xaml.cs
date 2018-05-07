83ce63001d8fe7ed702e9d24f119393f4101206f
sidleniwiec17 <piotrmichalik17@wp.pl>
2018-05-07 22:13:10 +0200

    beginning of part 2

---
+++ Biometria2/VoiceCode/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VoiceCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Voice voice1;
+        public Voice voice2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            voice1 = new Voice();
+            voice2 = new Voice();
+        }
+
+        private async void Load1_Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "audio";
+            dialog.Filter = "Wav files |*.wav;";
+
+            if (dialog.ShowDialog() == true)
+            {
+                BlakWait.Visibility = Visibility.Visible;
+                await LoadAudio1(dialog.FileName);
+                BlakWait.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void Load2_Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Title = "audio";
+            dialog.Filter = "Wav files |*.wav;";
+
+            if (dialog.ShowDialog() == true)
+            {
+                BlakWait.Visibility = Visibility.Visible;
+                await LoadAudio2(dialog.FileName);
+                BlakWait.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private async void Action_Button(object sender, RoutedEventArgs e)
+        {
+            if(voice1.NotNull() && voice2.NotNull())
+            {
+                BlakWait.Visibility = Visibility.Visible;
+                await Compare();
+                BlakWait.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                MessageBox.Show("At least one voice record is not loaded !");
+            }
+        }
+
+        private async Task LoadAudio1(string fileName)
+        {
+            await Task.Run(() =>
+            {
+                float[] tempLeft = null;
+                float[] tempRight = null;
+                if (Helper.readWav(fileName, out tempLeft, out tempRight))
+                {
+                    voice1.Left = tempLeft != null ? tempLeft : null;
+                    voice1.Right = tempRight != null ? tempRight : null;
+                }
+                else
+                {
+                    MessageBox.Show("Cannot load voice record !");
+                }
+            });
+        }
+
+        private async Task LoadAudio2(string fileName)
+        {
+            await Task.Run(() =>
+            {
+                float[] tempLeft = null;
+                float[] tempRight = null;
+                if (Helper.readWav(fileName, out tempLeft, out tempRight))
+                {
+                    voice2.Left = tempLeft != null ? tempLeft : null;
+                    voice2.Right = tempRight != null ? tempRight : null;
+                }
+                else
+                {
+                    MessageBox.Show("Cannot load voice record !");
+                }
+            });
+        }
+
+        private async Task Compare()
+        {
+            await Task.Run(() =>
+            {
+
+            });
+        }
+    }
+}
---
+++ Biometria2/VoiceCode/Voice.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoiceCode
+{
+    public class Voice
+    {
+        public float[] Left { get; set; }
+        public float[] Right { get; set; }
+        public Bitmap VoiceBitmap { get; set; }
+
+        public Voice()
+        {
+
+        }
+
+        public float[] GetFloatedSound()
+        {
+            if (Left != null)
+            {
+                return Left;
+            }
+            else if (Right != null)
+            {
+                return Right;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool NotNull()
+        {
+            if (Left != null)
+            {
+                return true;
+            }
+            else if (Right != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CreateBitmap()
+        {
+
+        }
+    }
+}
