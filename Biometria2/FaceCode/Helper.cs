a2251db4d2901440dfbc4642382cf27ed1cfd931
sidleniwiec17 <piotrmichalik17@wp.pl>
2018-05-20 18:45:08 +0200

    są czerwone mordki !

--- Biometria2/FaceCode/Helper.cs
+++ Biometria2/FaceCode/Helper.cs
@@ -37,7 +37,7 @@ namespace FaceCode
                 }
             }
             int treshold = (int)sum / pixAmount;
-            if(suggestedtreshold > 0)
+            if (suggestedtreshold > 0)
             {
                 treshold = suggestedtreshold;
             }
@@ -61,7 +61,7 @@ namespace FaceCode
                 {
                     byte[] oldColour;
                     oldColour = btm.getPixel(x, y);
-                    var value = CastColor( (byte)(Math.Abs((double)oldColour[1] - (double)oldColour[2])));
+                    var value = CastColor((byte)(Math.Abs((double)oldColour[1] - (double)oldColour[2])));
                     btm.setPixel(x, y, oldColour[0], (byte)value, (byte)value, (byte)value);
                 }
             }
@@ -99,10 +99,10 @@ namespace FaceCode
         {
             return color > 255 ? (byte)255 : (color < 0 ? (byte)0 : color);
         }
-        
+
         public static void LowPassFilter(ByteImage btm)
         {
-            int a = 4;
+            double a = 2.0;
             double[,] wages = new double[3, 3] { { 1, 1, 1 }, { 1, a, 1 }, { 1, 1, 1 } };
 
             for (int x = 0; x < btm.Width; x++)
@@ -142,7 +142,7 @@ namespace FaceCode
                 {
                     byte[] oldColour;
                     oldColour = btm.getPixel(x, y);
-                    var value = oldColour[1] < treshold ? (byte)(0.4 * (double)oldColour[1]) : (byte)(1.6 * (double)oldColour[1]);
+                    var value = oldColour[1] < treshold ? (byte)(0.1 * (double)oldColour[1]) : (byte)(1.5 * (double)oldColour[1]);
                     btm.setPixel(x, y, oldColour[0], (byte)value, (byte)value, (byte)value);
                 }
             }
@@ -182,23 +182,251 @@ namespace FaceCode
             return histogram;
         }
 
+
+        public static byte[] MarkFaceCenter(ByteImage btm, int X, int Y)
+        {
+            var currPix = btm.getPixel(X, Y);
+            byte[] newCol = new byte[] { currPix[0], (byte)220, (byte)0, (byte)0 };
+
+            btm.setPixel(X, Y, newCol);
+            btm.setPixel(X - 1, Y, newCol);
+            btm.setPixel(X + 1, Y, newCol);
+
+            btm.setPixel(X + 1, Y + 1, newCol);
+            btm.setPixel(X, Y - 1, newCol);
+            btm.setPixel(X, Y + 1, newCol);
+
+            btm.setPixel(X - 1, Y - 1, newCol);
+            btm.setPixel(X - 2, Y, newCol);
+            btm.setPixel(X + 2, Y, newCol);
+
+            btm.setPixel(X - 2, Y - 2, newCol);
+            btm.setPixel(X + 2, Y + 2, newCol);
+            btm.setPixel(X, Y - 2, newCol);
+            btm.setPixel(X, Y + 2, newCol);
+
+            return newCol;
+        }
+
+        public static void GaussFiltr(ByteImage btm)
+        {
+            double a = 2.0;
+            double[,] wages = new double[5, 5] { { 1, 1, 1, 1, 1 }, { 1, 1, a, 1, 1 }, { 1, a, a * a, a, 1 }, { 1, 1, a, 1, 1 }, { 1, 1, 1, 1, 1 } };
+
+            for (int x = 0; x < btm.Width; x++)
+            {
+                for (int y = 0; y < btm.Height; y++)
+                {
+                    var currpix = btm.getPixel(x, y);
+                    double sumR = 0.0;
+                    double sumG = 0.0;
+                    double sumB = 0.0;
+                    double dividor = 0.0;
+                    for (int x2 = -2; x2 < 3; x2++)
+                    {
+                        for (int y2 = -2; y2 < 3; y2++)
+                        {
+                            if (x + x2 >= 0 && y + y2 >= 0 && x + x2 < btm.Width && y + y2 < btm.Height)
+                            {
+                                double currWage = wages[x2 + 2, y2 + 2];
+                                var pix = btm.getPixel(x + x2, y + y2);
+                                sumR += (double)(pix[1]) * currWage;
+                                sumG += (double)(pix[2]) * currWage;
+                                sumB += (double)(pix[3]) * currWage;
+                                dividor += currWage;
+                            }
+                        }
+                    }
+                    btm.setPixel(x, y, currpix[0], CastColor((byte)(sumR / dividor)), CastColor((byte)(sumG / dividor)), CastColor((byte)(sumB / dividor)));
+                }
+            }
+        }
+
         public static int HorizontalCenter(int[] histogram)
         {
             int centerX = 0;
-           
+            var horizontalHistogram = Smooth(histogram);
+            horizontalHistogram = Smooth(horizontalHistogram);
+            horizontalHistogram = Smooth(horizontalHistogram);
+            horizontalHistogram = Smooth(horizontalHistogram);
+            horizontalHistogram = Smooth(horizontalHistogram);
+            horizontalHistogram = Smooth(horizontalHistogram);
+
+            int min = horizontalHistogram.Max();
+            int pos = 0;
+            for (int i = horizontalHistogram.Length / 3; i < horizontalHistogram.Length - (horizontalHistogram.Length / 3); i++)
+            {
+                if (horizontalHistogram[i] < min)
+                {
+                    pos = i;
+                    min = horizontalHistogram[i];
+                }
+            }
+            centerX = pos;
             return centerX;
         }
 
         public static int VerticalCenter(int[] histogram)
         {
             int centerY = 0;
+            var verticalHistogram = Smooth(histogram);
+            verticalHistogram = Smooth(verticalHistogram);
+            verticalHistogram = Smooth(verticalHistogram);
+            verticalHistogram = Smooth(verticalHistogram);
+            verticalHistogram = Smooth(verticalHistogram);
+            verticalHistogram = Smooth(verticalHistogram);
 
+            int min = verticalHistogram.Max();
+            int pos = 0;
+            for (int i = verticalHistogram.Length / 4; i < verticalHistogram.Length - (verticalHistogram.Length / 4); i++)
+            {
+                if (verticalHistogram[i] < min)
+                {
+                    pos = i;
+                    min = verticalHistogram[i];
+                }
+            }
+            centerY = pos;
             return centerY;
         }
 
         public static int[] Smooth(int[] histogram)
         {
-            return histogram;
+            int[] newHistogram = new int[histogram.Length];
+            List<int> currSum = new List<int>();
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                newHistogram[i] = 0;
+            }
+            currSum.Add(histogram[0]);
+            currSum.Add(histogram[1]);
+            currSum.Add(histogram[2]);
+            currSum.Add(histogram[3]);
+            currSum.Add(histogram[4]);
+            currSum.Add(histogram[3]);
+            currSum.Add(histogram[4]);
+
+            for (int i = 3; i < histogram.Length - 3; i++)
+            {
+                newHistogram[i] = (int)currSum.Sum() / currSum.Count;
+                currSum.RemoveAt(0);
+                if (i + 4 < histogram.Length)
+                {
+                    currSum.Add(histogram[i + 4]);
+                }
+            }
+            newHistogram[0] = newHistogram[3];
+            newHistogram[1] = newHistogram[3];
+            newHistogram[2] = newHistogram[3];
+            newHistogram[histogram.Length - 3] = newHistogram[histogram.Length - 4];
+            newHistogram[histogram.Length - 2] = newHistogram[histogram.Length - 4];
+            newHistogram[histogram.Length - 1] = newHistogram[histogram.Length - 4];
+            return newHistogram;
+        }
+
+        private static void FloodFill(ByteImage bmp, System.Drawing.Point pt, List<byte[]> targetColors, byte[] replacementColor)
+        {
+            List<Queue<System.Drawing.Point>> stackList = new List<Queue<System.Drawing.Point>>();
+            bool[][] isAdded = new bool[bmp.Width][];
+            for(int i = 0; i < isAdded.Length; i++)
+            {
+                isAdded[i] = new bool[bmp.Height];
+                for(int j = 0; j < bmp.Height; j++)
+                {
+                    isAdded[i][j] = false;
+                }
+            }
+            Queue<System.Drawing.Point> pixels = new Queue<System.Drawing.Point>();
+            //ByteImage tempPict = new ByteImage(bmp);
+            pixels.Enqueue(pt);
+            stackList.Add(pixels);
+            isAdded[pt.X][pt.Y] = true;
+            int lB = 0;
+            int rB = bmp.Width;
+            int uB = bmp.Height;
+            int bB = 0;
+                       
+            int stackIndex = 0;
+            while (ShouldContinue(stackList))
+            {
+                System.Drawing.Point a = stackList[stackIndex].Dequeue();
+               
+                if (a.X < rB && a.X >= lB &&
+                        a.Y < uB && a.Y >= bB)//make sure we stay within bounds
+                {
+                    bool isPushed = false;
+                    foreach (var c in targetColors)
+                    {
+                        if (!isPushed && bmp.getPixel(a.X, a.Y)[1] == c[1])
+                        {
+                            bmp.setPixel(a.X, a.Y, replacementColor);
+                            if (a.X - 1 < rB && a.X - 1 >= lB && a.Y < uB && a.Y >= bB && !isAdded[a.X - 1][a.Y])
+                            {
+                                stackList[stackIndex].Enqueue(new System.Drawing.Point(a.X - 1, a.Y));
+                                isAdded[a.X - 1][a.Y] = true;
+                            }
+                            if (a.X + 1 < rB && a.X + 1 >= lB && a.Y < uB && a.Y >= bB && !isAdded[a.X + 1][a.Y])
+                            {
+                                stackList[stackIndex].Enqueue(new System.Drawing.Point(a.X + 1, a.Y));
+                                isAdded[a.X + 1][a.Y] = true;
+                            }
+                            if (a.X < rB && a.X >= lB && a.Y - 1 < uB && a.Y - 1>= bB && !isAdded[a.X][a.Y - 1])
+                            {
+                                stackList[stackIndex].Enqueue(new System.Drawing.Point(a.X, a.Y - 1));
+                                isAdded[a.X][a.Y - 1] = true;
+                            }
+                            if (a.X < rB && a.X >= lB && a.Y + 1 < uB && a.Y + 1 >= bB && !isAdded[a.X][a.Y + 1])
+                            {
+                                stackList[stackIndex].Enqueue(new System.Drawing.Point(a.X, a.Y + 1));
+                                isAdded[a.X][a.Y + 1] = true;
+                            }
+                            isPushed = true;
+                        }
+                    }
+                }
+                if (stackList[stackIndex].Count > 30000)
+                {
+                    stackIndex++;
+                    stackList.Add(new Queue<System.Drawing.Point>());
+                    System.Drawing.Point b = stackList[stackIndex - 1].Dequeue();
+                    stackList[stackIndex].Enqueue(b);
+                }
+                else if (stackList[stackIndex].Count == 0)
+                {
+                    stackList.RemoveAt(stackIndex);
+                    stackIndex--;
+                }
+            }
+        }
+
+
+        private static bool ShouldContinue(List<Queue<System.Drawing.Point>> stackList)
+        {
+            bool isSomething = false;
+            for (int i = 0; i < stackList.Count; i++)
+            {
+                if (stackList[i].Count > 0)
+                {
+                    isSomething = true;
+                }
+            }
+            return isSomething;
+        }
+
+        private static bool CanAdd(List<Stack<System.Drawing.Point>> stackList, System.Drawing.Point point)
+        {
+            bool canAdd = true;
+            for (int i = 0; i < stackList.Count; i++)
+            {
+                for (int j = 0; j < stackList[i].Count; j++)
+                {
+                    if (stackList[i].ElementAt(j) == point)
+                    {
+                        canAdd = false;
+                    }
+                }
+            }
+            return canAdd;
         }
 
         public static void ProcessPicture(ByteImage picture)
@@ -207,18 +435,22 @@ namespace FaceCode
             int CenterY = 0;
             Test(picture);
             int treshold = Blackout(picture);
-            LowPassFilter(picture);
-            StretchColors(picture, treshold);
-            Binarization(picture, (int)(1.5 * (double)treshold));
+            GaussFiltr(picture);
+
+            StretchColors(picture, (int)(1.2 * (double)treshold));
+            Binarization(picture, (int)(1.6 * (double)treshold));
             int[] horizontalHistogram = GetHorizontalHistogram(picture);
             int[] verticalHistogram = GetVerticalHistogram(picture);
 
             //WYGLADZANIE !! moving average
             horizontalHistogram = Smooth(horizontalHistogram);
             verticalHistogram = Smooth(verticalHistogram);
-            //Znalezienie szczytów !! moje algo
+            //Znalezienie minimow !! moje algo
             CenterX = HorizontalCenter(horizontalHistogram);
             CenterY = VerticalCenter(verticalHistogram);
+
+            var newColor = MarkFaceCenter(picture, CenterX, CenterY);
+            FloodFill(picture, new System.Drawing.Point(CenterX, CenterY), new List<byte[]>() { new byte[] { 255, 255, 255, 255 }, newColor }, newColor);
         }
     }
 }
